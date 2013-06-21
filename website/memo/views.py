338e7dcef8dbd1190845268b1d261c2124dd331c
Wayne <guowangyang@gmail.com>
2013-06-21 14:17:59 +0800

    + memo: add home view

--- website/memo/views.py
+++ website/memo/views.py
@@ -1,1 +1,6 @@
-# Create your views here.
+# _*_ coding: utf8 _*_
+from django.views.generic.base import TemplateView
+
+
+class HomeView(TemplateView):
+    template_name = 'home.html'
--- website/website/urls.py
+++ website/website/urls.py
@@ -1,17 +1,11 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns, url
+import memo.views
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'website.views.home', name='home'),
-    # url(r'^website/', include('website.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+urlpatterns = patterns(
+    '',
+    url(r'^$', memo.views.HomeView.as_view(), name='home'),
 )
