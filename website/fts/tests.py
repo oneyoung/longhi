3c393757b6222d38d8566ea9de5d4c61a59eca71
Oneyoung@Acer4710 <guowangyang@gmail.com>
2013-06-22 22:57:09 +0800

    + fts: function test for register page

--- website/fts/tests.py
+++ website/fts/tests.py
@@ -5,6 +5,7 @@ when you run "manage.py test".
 Replace this with more appropriate tests for your application.
 """
 from django.test import LiveServerTestCase
+from django.core.urlresolvers import reverse
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
@@ -31,3 +32,34 @@ class AccountTest(LiveServerTestCase):
 
         # TODO: use the admin site to create a Poll
         self.fail('finish this test')
+
+    def test_user_register(self):
+        def fill_register_form(username, password, password_confirm=''):
+            # open login page
+            self.browser.get(self.live_server_url + reverse('memo.views.register'))
+            # fill in the email filed
+            tag = self.browser.find_element(By.NAME, 'username')
+            tag.send_keys(username)
+            # input the passowrd
+            tag = self.browser.find_element(By.NAME, 'passowrd')
+            tag.send_keys(password)
+            # comfirm the passowrd
+            tag = self.browser.find_element(By.NAME, 'passowrd_confirm')
+            tag.send_keys(password_confirm if password_confirm else password)
+            # submit
+            tag.submit()
+
+        username = 'xxxx@example.com'
+        password = 'xxxxxxxx'
+
+        # new user register
+        fill_register_form(username, password)
+        # should say that you are successful.
+        body = self.browser.find_element(By.NAME, 'body')
+        self.assertIn('Successful', body.txt)
+
+        # repeat register should failed if the same username
+        fill_register_form(username, password)
+        # should say account has exists
+        body = self.browser.find_element(By.NAME, 'body')
+        self.assertIn('exist', body.txt)
