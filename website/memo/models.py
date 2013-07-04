33ec9a5a6ba699633a27c26aa01f828c180825de
Wayne <guowangyang@gmail.com>
2013-07-04 16:17:07 +0800

    + memo.models: time realted fields for Setting

--- website/memo/models.py
+++ website/memo/models.py
@@ -46,8 +46,44 @@ class Statics(models.Model):
 class Setting(models.Model):
     user = models.OneToOneField(User)
     nickname = models.CharField(max_length=100)
-    interval = models.IntegerField(default=1)
     markdown = models.BooleanField(default=False)
+    TIME_ZONE_CHOICES = (
+        ('-12.0', '(GMT -12:00) Eniwetok, Kwajalein'),
+        ('-11.0', '(GMT -11:00) Midway Island, Samoa'),
+        ('-10.0', '(GMT -10:00) Hawaii'),
+        ('-9.0', '(GMT -9:00) Alaska'),
+        ('-8.0', '(GMT -8:00) Pacific Time (US & Canada)'),
+        ('-7.0', '(GMT -7:00) Mountain Time (US & Canada)'),
+        ('-6.0', '(GMT -6:00) Central Time (US & Canada), Mexico City'),
+        ('-5.0', '(GMT -5:00) Eastern Time (US & Canada), Bogota, Lima'),
+        ('-4.0', '(GMT -4:00) Atlantic Time (Canada), Caracas, La Paz'),
+        ('-3.5', '(GMT -3:30) Newfoundland'),
+        ('-3.0', '(GMT -3:00) Brazil, Buenos Aires, Georgetown'),
+        ('-2.0', '(GMT -2:00) Mid-Atlantic'),
+        ('-1.0', '(GMT -1:00) Azores, Cape Verde Islands'),
+        ('0.0', '(GMT) Western Europe Time, London, Lisbon, Casablanca'),
+        ('1.0', '(GMT +1:00) Brussels, Copenhagen, Madrid, Paris'),
+        ('2.0', '(GMT +2:00) Kaliningrad, South Africa'),
+        ('3.0', '(GMT +3:00) Baghdad, Riyadh, Moscow, St. Petersburg'),
+        ('3.5', '(GMT +3:30) Tehran'),
+        ('4.0', '(GMT +4:00) Abu Dhabi, Muscat, Baku, Tbilisi'),
+        ('4.5', '(GMT +4:30) Kabul'),
+        ('5.0', '(GMT +5:00) Ekaterinburg, Islamabad, Karachi, Tashkent'),
+        ('5.5', '(GMT +5:30) Bombay, Calcutta, Madras, New Delhi'),
+        ('5.75', '(GMT +5:45) Kathmandu'),
+        ('6.0', '(GMT +6:00) Almaty, Dhaka, Colombo'),
+        ('7.0', '(GMT +7:00) Bangkok, Hanoi, Jakarta'),
+        ('8.0', '(GMT +8:00) Beijing, Perth, Singapore, Hong Kong'),
+        ('9.0', '(GMT +9:00) Tokyo, Seoul, Osaka, Sapporo, Yakutsk'),
+        ('9.5', '(GMT +9:30) Adelaide, Darwin'),
+        ('10.0', '(GMT +10:00) Eastern Australia, Guam, Vladivostok'),
+        ('11.0', '(GMT +11:00) Magadan, Solomon Islands, New Caledonia'),
+        ('12.0', '(GMT +12:00) Auckland, Wellington, Fiji, Kamchatka'),
+    )
+    timezone = models.CharField(default='8.0', max_length=10, choices=TIME_ZONE_CHOICES)
+    preferhour = models.IntegerField(default=20)
+    interval = models.IntegerField(default=1)
+    notify = models.BooleanField(default=False)
 
 
 # signals here
