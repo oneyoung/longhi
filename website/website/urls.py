ea731edeea476be89c2789ea103f27cf1b42c120
Wayne <guowangyang@gmail.com>
2013-07-19 11:46:32 +0800

    * website: use string to index modules in urls.py

--- website/website/urls.py
+++ website/website/urls.py
@@ -1,5 +1,4 @@
 from django.conf.urls import patterns, url
-import memo.views
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -7,17 +6,17 @@ import memo.views
 
 urlpatterns = patterns(
     '',
-    url(r'^$', memo.views.home),
-    url(r'user/register/$', memo.views.register),
-    url(r'user/login/$', memo.views.login),
-    url(r'user/logout/$', memo.views.logout),
-    url(r'user/suicide/$', memo.views.suicide),
-    url(r'memo/io/$', memo.views.memo_io),
-    url(r'memo/ajax/$', memo.views.memo_ajax),
-    url(r'memo/write/$', memo.views.memo_write),
-    url(r'memo/entry/$', memo.views.memo_entry),
-    url(r'setting/$', memo.views.memo_setting),
-    url(r'setting/unsubscribe/(?P<keys>\S+)/$', memo.views.unsubscribe),
-    url(r'activate/(?P<keys>\S+)/$', memo.views.activate),
-    url(r'mailbox/$', memo.views.mailbox, name='mailbox'),
+    url(r'^$', 'memo.views.home'),
+    url(r'user/register/$', 'memo.views.register'),
+    url(r'user/login/$', 'memo.views.login'),
+    url(r'user/logout/$', 'memo.views.logout'),
+    url(r'user/suicide/$', 'memo.views.suicide'),
+    url(r'memo/io/$', 'memo.views.memo_io'),
+    url(r'memo/ajax/$', 'memo.views.memo_ajax'),
+    url(r'memo/write/$', 'memo.views.memo_write'),
+    url(r'memo/entry/$', 'memo.views.memo_entry'),
+    url(r'setting/$', 'memo.views.memo_setting'),
+    url(r'setting/unsubscribe/(?P<keys>\S+)/$', 'memo.views.unsubscribe'),
+    url(r'activate/(?P<keys>\S+)/$', 'memo.views.activate'),
+    url(r'mailbox/$', 'memo.views.mailbox', name='mailbox'),
 )
